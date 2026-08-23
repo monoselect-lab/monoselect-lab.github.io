3d9e9b45160ee634bb40657a7983e6b35c622386
nogi <kokurakura@icloud.com>
2026-08-23 17:11:36 +0900

    Initial site: comparison blog setup + first article

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'モノセレクトラボ';
+export const SITE_DESCRIPTION = '悩み・シーン別に商品を比較して選び方を紹介するブログです。';
